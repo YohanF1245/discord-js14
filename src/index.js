373f1728bdbd25883b8771214b8e137ade5d703d
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-10-05 19:21:14 +0200

    feat(index.js): bot scan messages and replies on selected keyword

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ const client = new Client({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildModeration
     ]
 });
+
+client.on('ready', () => {
+    console.log(`✅Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content.toLocaleLowerCase() === 'putain' || message.content.toLocaleLowerCase() === 'bite') {
+        message.reply('Oh ! Surveilles ton language, '+message.author.displayName+" !");
+    }
+    if(message.content.toLocaleLowerCase() === 'salut' || message.content.toLocaleLowerCase() === 'bonjour')
+        if(!message.author.bot){
+            message.reply('Salut, '+message.author.displayName+" !");
+        }
+    })
+
+client.login('');
+
