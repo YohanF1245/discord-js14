b0fb2956e83dbe10bd8bbdd4b31a58637042c1f3
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-10-05 20:19:01 +0200

    feat(register-commands.js): addition slash command register

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 
-const { REST, Routes } = require('discord.js');
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 
 const commands  = [
     {
         name: 'hey',
         description: 'Reply with hey',
+    },
+    {
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'first-number',
+                description: 'First number',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            },
+            {
+                name: 'second-number',
+                description: 'Second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            }   
+        ]
     }
 ];
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
