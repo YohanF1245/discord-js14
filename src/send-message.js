de622cafd4123e7d7cbbdf9127c21c978f87619d
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-10-06 12:05:33 +0200

    chore(send-message.js): add file for role selection button logic

---
+++ src/send-message.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+
+
+client.on('ready', () => {
+    console.log(`✅Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
